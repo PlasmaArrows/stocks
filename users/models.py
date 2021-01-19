ddb6007c966ae6e27007f44cc3a285e12219fe84
Michael Tang <michaeltang@Michaels-MBP-3.fios-router.home>
2021-01-19 16:20:27 -0500

    First

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    money = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Stocks(models.Model):
+    ticker = models.CharField(max_length=4)
+    boughtTime = models.DateTimeField()
+    owned_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ticker
+
